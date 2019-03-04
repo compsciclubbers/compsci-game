9df07e8db08bd94a7b6b807a40317d1b900a5541
Justin Williams <justinwilliams2100@gmail.com>
2019-03-04 14:04:18 -0500

    Inventory better

--- Assets/Justin/inventoryScript.cs
+++ Assets/Justin/inventoryScript.cs
@@ -8,12 +8,15 @@ public class inventoryScript : MonoBehaviour
     private int allSlots;
     private int enabledSlots;
     private GameObject[] slot;
+    private Transform child;
+    private int currIndex = 0;
+    private double startTime;
+    private double curr;
     void Start()
     {
         inventory.SetActive(false);
         allSlots = 9;
         slot = new GameObject[allSlots];
-
         for (int i = 0; i < allSlots; i++)
         {
             slot[i] = inventory.transform.GetChild(i).gameObject;
@@ -22,10 +25,56 @@ public class inventoryScript : MonoBehaviour
                 slot[i].GetComponent<Slot>().empty = true;
             }
         }
+        startTime = Time.time;
     }
 
     void Update()
     {
+        if (inventory.active)
+        {
+            child = slot[currIndex].transform.GetChild(0).GetChild(0);
+            child.gameObject.SetActive(true);
+            for (int i = 0; i < allSlots; i++)
+            {
+                if(i != currIndex)
+                {
+                    slot[i].transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
+                }
+            }
+            curr = Time.time - startTime;
+            if (Input.GetKey("left") && curr > .1)
+            {
+                if (currIndex == 0)
+                {
+                    currIndex = allSlots - 1;
+                }
+                else
+                {
+                    currIndex--;
+                }
+                startTime = Time.time;
+            }
+            if (Input.GetKey("right") && curr > .1)
+            {
+                if (currIndex == allSlots - 1)
+                {
+                    currIndex = 0;
+                }
+                else
+                {
+                    currIndex++;
+                }
+                startTime = Time.time;
+            }
+            if (Input.GetKey("return"))
+            {
+                print(currIndex);
+            }
+            if (Input.GetKey("return") && slot[currIndex].transform.GetChildCount() > 1)
+            { 
+                slot[currIndex].transform.GetChild(1).gameObject.GetComponent<Item>().itemUsage();
+            }
+        }
         if (Input.GetKeyDown(KeyCode.I))
         {
             inventory.SetActive(!inventory.active);
