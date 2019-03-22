9c7809cc8b29490a39c24970522a56c920b2d0f5
Stephen Welch <stephenwelchva@gmail.com>
2019-03-22 17:04:16 -0400

    Add back missing scripts

---
+++ Assets/Framework/BasicStateMachine.cs
@@ -0,0 +1,47 @@
+﻿
+/**
+ * Implements a basic state machine. handle() methods are expected to be called by the subclass.
+ * **/
+public abstract class BasicStateMachine<S> : UnityEngine.MonoBehaviour where S : System.Enum
+{
+
+    private S lastState, currentState;
+
+    public BasicStateMachine(S initialState)
+    {
+        lastState = initialState;
+        currentState = initialState;
+    }
+
+    protected abstract S HandleRequestedState(S requestedState);
+    protected abstract void HandleCurrentState(S currentState);
+    
+    public void RequestState(S requestedState)
+    {
+        if(!requestedState.Equals(currentState))
+        {
+            currentState = HandleRequestedState(requestedState);
+        }
+    }
+
+    public void Reset(S state)
+    {
+        lastState = state;
+        currentState = state;
+    }
+
+    public void SetCurrentState(S state)
+    {
+        this.currentState = state;
+    }
+
+    public S GetCurrentState()
+    {
+        return currentState;
+    }
+
+    public S getLastState()
+    {
+        return lastState;
+    }
+}
---
+++ Assets/Framework/CharacterController.cs
@@ -0,0 +1,50 @@
+﻿// External release version 2.0.0
+
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+/// <summary>
+/// Custom character controller, to be used by attaching the component to an object
+/// and writing scripts attached to the same object that recieve the "SuperUpdate" message
+/// </summary>
+public class CharacterController : MonoBehaviour
+{
+
+    private readonly int kPoseBufferSize = 2000;
+    private readonly Vector3 kCollisionVector = new Vector3(-1, -1, -1);
+
+    private Queue<TimestampedVector3> mPoseHistory = new Queue<TimestampedVector3>();
+    private Boolean mIsColliding = false;
+
+    private void FixedUpdate()
+    {
+        mPoseHistory.Enqueue(TimestampedVector3.CreateTimestampedVector3(this));
+        if(mPoseHistory.Count > kPoseBufferSize)
+        {
+            mPoseHistory.Dequeue();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        mIsColliding = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        mIsColliding = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        this.transform.position = mPoseHistory.Dequeue().GetVector3();
+    }
+
+    public Boolean IsColliding()
+    {
+        return mIsColliding;
+    }
+
+}
